7f5d0180b6b598408d286166173eb105f1f4be16
null-heap <142169162+null-heap@users.noreply.github.com>
2024-03-25 19:42:37 +0200

    feat:made the game work description: attached the js game mechanism to the html elements

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ function playRound(playerSelection, computerSelection){
     console.log("playerSelection = " + playerSelection)
     console.log("computerSelection = " + computerSelection)
 
-    if(playerSelection == computerSelection)
+    if(playerSelection == computerSelection) //if a tie occurs
     {
         return 2;
     }
@@ -35,38 +35,77 @@ function playRound(playerSelection, computerSelection){
     if(playerSelection == "scissors" & computerSelection == "paper"){
         return 1;
     }
-    return 3;
+    return 3;//if incorrect input occurs
 }
 
-function playGame(rounds){
     let playerScore = 0;
     let computerScore = 0;
-    for(let i=0; i< rounds;i++){
-        console.log(`round - **${i + 1}**`)
-        let playerSelection = prompt("chose your firearm");
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection,computerSelection);
+    let buttons = document.querySelector('#buttons');
+    let playerSelection = "";
+    let round = document.querySelector('#round')
+    round.textContent = "I work";
+    let roundCount = 0;
+    let playerResult = document.querySelector('#playerResult');
+    let computerResult = document.querySelector('#computerResult');
+    let roundResult = document.querySelector('#roundResult');
+    let endResult = document.createElement("h2");
 
-        if(result==1 ){
-         console.log(`player wins, ${playerSelection} beats ${computerSelection}`)
-         playerScore++;
-        }else if(result == 0){
-            console.log(`computer wins!!!, ${computerSelection} beats ${playerSelection}`)
-            computerScore++;
-        }else if( result== 2){
-            console.log("its a tie")
-        }else{
-            console.log("**incorrect input by user**")
-        }
-    }
-    console.log(`player Score =${playerScore}, computer Score =${computerScore}`)
+        
+        
 
-    if(playerScore == computerScore){
-    console.log("ITS a TIE! NO Winners") 
-    }else if(playerScore < computerScore){
-        console.log("Computer Winns!") 
-    }else{
-        console.log("Player Wins.")
-    }
-}
-playGame(5);
+        buttons.addEventListener('click', (event) => {
+            roundCount ++;
+            round.textContent = `round: **${roundCount}**`;
+            
+            let target = event.target;
+            switch(target.id){
+                case 'rock':
+                    playerSelection = 'rock';
+                    break;
+                case 'paper':
+                    playerSelection = 'paper';
+                    break;
+                case 'scissors':
+                    playerSelection = 'scissors';
+                    break;
+            }
+        
+
+            let computerSelection = getComputerChoice();
+            let result = playRound(playerSelection,computerSelection);
+
+            if(result==1 ){
+                roundResult.textContent = `player win the round, ${playerSelection} beats ${computerSelection}`;
+                playerScore++;
+            }else if(result == 0){
+                roundResult.textContent = `computer win the round, ${computerSelection} beats ${playerSelection}`;
+                computerScore++;
+            }else if( result == 2){
+                roundResult.textContent = "its a tie";
+            }
+            playerResult.textContent = 'player Score: ' + playerScore;
+            computerResult.textContent = 'computer Score: ' + computerScore;
+            if(playerScore >= 5 || computerScore >= 5){
+                if(playerScore == computerScore){
+                    endResult.textContent = "ITS a TIE! NO Winners";
+                }else if(playerScore < computerScore){
+                    endResult.textContent = "Computer Wins the game!";
+                }else{
+                    endResult.textContent = "Player Wins the game";
+                }
+
+                roundResult.appendChild(endResult);
+
+            roundCount = 0;
+            computerScore = 0;
+            playerScore = 0;
+            playerResult.textContent = 'player Score: ' + 0;
+            computerResult.textContent = 'computer Score: ' + 0;
+            round.textContent = '0';
+            roundResult.text = "Begin Round";
+            }
+    })
+
+
+
+
